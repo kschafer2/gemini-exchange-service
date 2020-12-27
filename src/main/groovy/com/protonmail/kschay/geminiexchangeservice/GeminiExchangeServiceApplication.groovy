6adf3a182946b072f6c2a2d6a19f101e53e64a73
Kyle Schafer <kschay@protonmail.com>
2020-12-26 22:32:56 -0600

    Initial commit

---
+++ src/main/groovy/com/protonmail/kschay/geminiexchangeservice/GeminiExchangeServiceApplication.groovy
@@ -0,0 +1,13 @@
+package com.protonmail.kschay.geminiexchangeservice
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class GeminiExchangeServiceApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(GeminiExchangeServiceApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/protonmail/kschay/geminiexchangeservice/GeminiExchangeServiceApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.protonmail.kschay.geminiexchangeservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GeminiExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
