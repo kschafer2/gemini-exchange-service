38ad86e2a4f4d54d8bf4a6f4f0e27e1311e12d94
Kyle Schafer <kschay@protonmail.com>
2021-01-13 18:56:23 -0700

    Merged ServletInitializer into GeminiExchangeServiceApplication

--- src/main/groovy/com/protonmail/kschay/geminiexchangeservice/GeminiExchangeServiceApplication.groovy
+++ src/main/groovy/com/protonmail/kschay/geminiexchangeservice/GeminiExchangeServiceApplication.groovy
@@ -2,9 +2,16 @@ package com.protonmail.kschay.geminiexchangeservice
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
-class GeminiExchangeServiceApplication {
+class GeminiExchangeServiceApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		application.sources(GeminiExchangeServiceApplication)
+	}
 
 	static void main(String[] args) {
 		SpringApplication.run(GeminiExchangeServiceApplication, args)
