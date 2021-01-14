22a48ce4a7300e31a5fb7a243264eece1b83905c
Kyle Schafer <kschay@protonmail.com>
2021-01-13 18:58:43 -0700

    Migrated more methods from crypto-trader

--- src/main/groovy/com/protonmail/kschay/geminiexchangeservice/domain/Symbol.groovy
+++ src/main/groovy/com/protonmail/kschay/geminiexchangeservice/domain/Symbol.groovy
@@ -28,4 +28,36 @@ enum Symbol {
     LINKETH,
     OXTETH,
     LINKUSD
+
+    Currency base() {
+        final def symbolString = toString()
+
+        if(symbolString.length() == 6) {
+            return Currency.valueOf(symbolString.substring(0, 3))
+        }
+        else if(symbolString.length() == 7) {
+            try {
+                return Currency.valueOf(symbolString.substring(0, 4))
+            } catch(IllegalArgumentException ignored) {
+                return Currency.valueOf(symbolString.substring(0, 3))
+            }
+        }
+        return null
+    }
+
+    Currency quote() {
+        final def symbolString = toString()
+
+        if(symbolString.length() == 6) {
+            return Currency.valueOf(symbolString.substring(3))
+        }
+        else if(symbolString.length() == 7) {
+            try {
+                return Currency.valueOf(symbolString.substring(4))
+            } catch(IllegalArgumentException ignored) {
+                return Currency.valueOf(symbolString.substring(3))
+            }
+        }
+        return null
+    }
 }
